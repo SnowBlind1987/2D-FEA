f9c0ecce644b5a19b23a71664cf0942e25b24a1a
aandreyev <andreyev.andrey@gmail.com>
2016-08-01 23:24:38 -0400

    added geometry function and jacobian calculation

--- moduleElementFunctions.f95
+++ moduleElementFunctions.f95
@@ -1,7 +1,10 @@
+module ShapeFunctions
+use module SetPrecision
+contains
 function elementQuadFunction(ksi,eta,nfunc) result(N)
-real, intent(in)::ksi,eta
+real(prec), intent(in)::ksi,eta
 integer,intent(in)::nfunc
-
+real(prec),parameter::fourth=1.0_prec/4.0_prec
 select case(nfunc)
 
 case(1)
@@ -17,4 +20,8 @@ case(4)
 	ksiI=-1.0
 	etaI=1.0
 
-N=0.5*(1+ksi*ksiI)*(1+eta*etaI)
+N=0.25*(1+ksi*ksiI)*(1+eta*etaI)
+
+end function
+
+end module ShapeFunctions
---
+++ moduleGeometry.f95
@@ -0,0 +1,26 @@
+module Geometry
+use modulePrecision
+contains
+type Node
+	integer:: nid
+	real(prec):: x,y,z
+end type Node
+
+type QuadElement
+	integer::eid
+	type(node)::n1
+	type(node)::n2
+	type(node)::n3
+	type(node)::n4
+end type Element
+
+subroutine calcJacobian(X1,X2,X3,X4,&
+						Y1,Y2,Y3,Y3,jac)
+real(prec),intent(in)::X1,X2,X3,X4
+real(prec),intent(in)::Y1,Y2,Y3,Y4
+real(prec),intent(out)::jac
+real(prec),parameter::eighth=1.0_prec/8.0_prec
+jac=eighth*(X1*Y2-X1*Y4-X2*Y1+X2*Y4-X3*Y2+X3*Y4+X4*Y1-X4*Y3)
+
+
+end module
